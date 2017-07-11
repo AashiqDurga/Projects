7f5c15bde004ccb245d0d83728f49c2cc448a12a
Adriaan Putter <adriaan.putter@gmail.com>
2017-07-11 09:02:53 +0200

    Added dotnet core service relay thing

---
+++ poster/Controllers/PosterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace poster.Controllers
+{
+    [Route("api/[controller]")]
+    public class SensorController : Controller
+    {
+        private static IEnumerable<SensorInfo> _sensorInfos = new List<SensorInfo>() {
+            new SensorInfo() {Name = "My Test", Status = "Failed" }};
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<SensorInfo> Get()
+        {
+            return _sensorInfos;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]IEnumerable<SensorInfo> sensorInfos)
+        {
+            _sensorInfos = sensorInfos;
+        }
+    }
+
+    public class SensorInfo
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
